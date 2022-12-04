96e12c6565a28029cdf53830ff2fac2a3263c3e3
Raphael Sprenger <raphael@nosuchcode.com>
2022-12-04 13:41:41 +0100

    pretty-print solutions

--- main.go
+++ main.go
@@ -3,12 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(day1_1(readDayN(1)))
-	fmt.Println(day1_2(readDayN(1)))
-	fmt.Println(day2_1(readDayN(2)))
-	fmt.Println(day2_2(readDayN(2)))
-	fmt.Println(day3_1(readDayN(3)))
-	fmt.Println(day3_2(readDayN(3)))
-	fmt.Println(day4_1(readDayN(4)))
-	fmt.Println(day4_2(readDayN(4)))
+	type aoc struct {
+		day          int
+		task1, task2 func(string) int
+	}
+
+	days := []aoc{
+		{
+			day:   1,
+			task1: day1_1,
+			task2: day1_2,
+		},
+		{
+			day:   2,
+			task1: day2_1,
+			task2: day2_2,
+		},
+		{
+			day:   3,
+			task1: day3_1,
+			task2: day3_2,
+		},
+		{
+			day:   4,
+			task1: day4_1,
+			task2: day4_2,
+		},
+	}
+
+	for _, d := range days {
+		input := readDayN(d.day)
+		fmt.Printf("Day %v\n", d.day)
+		if d.task1 != nil {
+			fmt.Printf("Task1: %v\n", d.task1(input))
+		}
+		if d.task2 != nil {
+			fmt.Printf("Task2: %v\n", d.task2(input))
+		}
+		fmt.Println("-----------------")
+	}
 }
